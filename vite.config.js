4242c164ae55c5cac3988c04def8d4f8de918cae
WavyWare <jflortech@gmail.com>
2024-10-20 12:35:43 +0200

    Deploy on github pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
  
 export default defineConfig({
+  base: "/Chemistry-Table/",
   plugins: [react()],
   resolve: {
     alias: {
